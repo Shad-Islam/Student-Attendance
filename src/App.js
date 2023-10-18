97e93ef5d4b76b37cf30adeb0a1d4ec5793059d7
Shad-islam <shadislam48@gmail.com>
2023-10-18 21:48:59 +0600

    Add condition for taking student name

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import "./App.css";
 
 function App() {
   const [edit, setEdit] = useState(false);
+  const [studentName, setStudentName] = useState();
+
+  const submitHandeler = (studentName) => {
+    setStudentName(studentName);
+  };
 
   return (
     <>
       <div className="student-form-container">
         <form className="student-form">
-          <input type="text" />
+          <input
+            type="text"
+            value={studentName}
+            onChange={(e) => {
+              submitHandeler(e.target.value);
+            }}
+          />
+          {console.log(studentName)}
+
           <button type="submit">
             {edit ? "Update Student" : "Add Student"}
           </button>
