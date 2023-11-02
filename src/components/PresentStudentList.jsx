b40f54dc38982eb6e49a7dd65482a7405e7335cb
Shad-islam <shadislam48@gmail.com>
2023-11-03 03:27:22 +0600

    Add present student list

--- src/components/PresentStudentList.jsx
+++ src/components/PresentStudentList.jsx
@@ -1,29 +1,29 @@
-// import React from "react";
+import React from "react";
 
-// function PresentStudentList(props) {
-//   return (
-//     <>
-//       <div className="list presentStudentList">
-//         <h2>Present Student List</h2>
-//         <ul>
-//           {props.studentList
-//             .filter((student) => student.isPresent === true)
-//             .map((item) => (
-//               <li key={item.id}>
-//                 <span>{item.name}</span>
-//                 <button
-//                   onClick={() => {
-//                     props.toggleHandler(item);
-//                   }}
-//                 >
-//                   Accidentally Added
-//                 </button>
-//               </li>
-//             ))}
-//         </ul>
-//       </div>
-//     </>
-//   );
-// }
+function PresentStudentList(props) {
+  return (
+    <>
+      <div className="list presentStudentList">
+        <h2>Present Student List</h2>
+        <ul>
+          {props.studentList
+            .filter((student) => student.isPresent === true)
+            .map((item) => (
+              <li key={item.id}>
+                <span>{item.name}</span>
+                <button
+                  onClick={() => {
+                    props.toggleHandler(item);
+                  }}
+                >
+                  Accidentally Added
+                </button>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
+}
 
-// export default PresentStudentList;
+export default PresentStudentList;
