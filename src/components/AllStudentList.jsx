cd651eeed43420a8d3fbc67e2b4a8c24f1e4da88
Shad-islam <shadislam48@gmail.com>
2023-11-07 23:49:05 +0600

    Add handler on context

--- src/components/AllStudentList.jsx
+++ src/components/AllStudentList.jsx
@@ -4,47 +4,7 @@ import { StudentContext } from "../context/StudentProvider";
 
 function AllStudentList() {
   const StudentContextValue = useContext(StudentContext);
-  // Edit student
-  const editHandler = (student) => {
-    StudentContextValue.setEditMode(true);
-    StudentContextValue.setStudentName(student.name);
-    StudentContextValue.setEditAbleStudent(student);
-  };
 
-  // Remove student
-  const removeHandler = (studentId) => {
-    const newStudentList = StudentContextValue.studentList.filter(
-      (student) => studentId !== student.id
-    );
-    StudentContextValue.setStudentList(newStudentList);
-  };
-
-  // make present
-  const makePresentHandler = (student) => {
-    if (student.isPresent === true || student.isPresent === false) {
-      return alert("THe student is already in a list");
-    }
-    const newStudentList = StudentContextValue.studentList.map((item) => {
-      if (student.id === item.id) {
-        return { ...item, isPresent: true };
-      }
-      return item;
-    });
-    StudentContextValue.setStudentList(newStudentList);
-  };
-  // make absent
-  const makeAbsentHandler = (student) => {
-    if (student.isPresent === true || student.isPresent === false) {
-      return alert("THe student is already in a list");
-    }
-    const newStudentList = StudentContextValue.studentList.map((item) => {
-      if (item.id === student.id) {
-        return { ...item, isPresent: false };
-      }
-      return item;
-    });
-    StudentContextValue.setStudentList(newStudentList);
-  };
   return (
     <>
       <div className="list allStudentList">
@@ -55,22 +15,26 @@ function AllStudentList() {
               <span>{student.name}</span>
               <button
                 onClick={() => {
-                  editHandler(student);
+                  StudentContextValue.editHandler(student);
                 }}
               >
                 edit
               </button>
               <button
                 onClick={() => {
-                  removeHandler(student.id);
+                  StudentContextValue.removeHandler(student.id);
                 }}
               >
                 remove
               </button>
-              <button onClick={() => makePresentHandler(student)}>
+              <button
+                onClick={() => StudentContextValue.makePresentHandler(student)}
+              >
                 make present
               </button>
-              <button onClick={() => makeAbsentHandler(student)}>
+              <button
+                onClick={() => StudentContextValue.makeAbsentHandler(student)}
+              >
                 make absent
               </button>
             </li>
--- src/context/StudentProvider.jsx
+++ src/context/StudentProvider.jsx
@@ -4,10 +4,54 @@ import { createContext } from "react";
 export const StudentContext = createContext();
 
 const StudentProvider = ({ children }) => {
+  // states
   const [studentName, setStudentName] = useState("");
   const [editMode, setEditMode] = useState(false);
   const [studentList, setStudentList] = useState([]);
   const [editAbleStudent, setEditAbleStudent] = useState(null);
+
+  // handlers
+  // Edit student
+  const editHandler = (student) => {
+    setEditMode(true);
+    setStudentName(student.name);
+    setEditAbleStudent(student);
+  };
+
+  // Remove student
+  const removeHandler = (studentId) => {
+    const newStudentList = studentList.filter(
+      (student) => studentId !== student.id
+    );
+    setStudentList(newStudentList);
+  };
+
+  // make present
+  const makePresentHandler = (student) => {
+    if (student.isPresent === true || student.isPresent === false) {
+      return alert("THe student is already in a list");
+    }
+    const newStudentList = studentList.map((item) => {
+      if (student.id === item.id) {
+        return { ...item, isPresent: true };
+      }
+      return item;
+    });
+    setStudentList(newStudentList);
+  };
+  // make absent
+  const makeAbsentHandler = (student) => {
+    if (student.isPresent === true || student.isPresent === false) {
+      return alert("THe student is already in a list");
+    }
+    const newStudentList = studentList.map((item) => {
+      if (item.id === student.id) {
+        return { ...item, isPresent: false };
+      }
+      return item;
+    });
+    setStudentList(newStudentList);
+  };
 
   const contextValue = {
     editMode,
@@ -18,6 +62,10 @@ const StudentProvider = ({ children }) => {
     setStudentName,
     editAbleStudent,
     setEditAbleStudent,
+    editHandler,
+    removeHandler,
+    makeAbsentHandler,
+    makePresentHandler,
   };
   return (
     <StudentContext.Provider value={contextValue}>
